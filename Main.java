2e0ec56899cf6be9f182789ff242006331a91321
Gyor <gyor@egb080000007.lancs.local>
2021-10-13 14:48:52 +0100

    Added the Mars and the Moon

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         SolarSystem solarSystem = new SolarSystem(1600, 1000);
-        
-        //solarSystem.drawSolarObject(240, 210, 20, "BLUE"); // this and the below line are basically doing the same thing
-        
+
+        int j = 0;
         for(int i = 0; i < 720; i++){
             solarSystem.drawSolarObject(0, 0, 80, "YELLOW");
             solarSystem.drawSolarObjectAbout(150, i, 20, "BLUE", 0, 0);
+            solarSystem.drawSolarObjectAbout(200, i-50, 15, "RED", 0, 0);
+            solarSystem.drawSolarObjectAbout(20, -i, 10, "WHITE", 150, i);
             solarSystem.finishedDrawing();
         }
         
