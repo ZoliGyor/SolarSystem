ea7cdf9fc7ca8a5d0dcce9199246e3fe91c9fc47
Gyor <gyor@egb080000011.lancs.local>
2021-10-20 13:14:56 +0100

    Created the Sun class

---
+++ Sun.java
@@ -0,0 +1,11 @@
+public class Sun {
+    private double diamaeter;
+    private String colour;
+
+    public Sun(double diamaeter, String colour){
+        this.diamaeter = diamaeter;
+        this.colour = colour;
+    }
+
+    
+}
